d800af2657c84e450c97aaafaef9a45bf1e0939a
Estrella Popoca <65576812+exrlla@users.noreply.github.com>
2020-08-22 11:58:14 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+//Author: Estrella Popoca
+//Date Created: August 20th, 2020
+
+public class Main
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("From: Bill Smith" +
+		 "                                      ");
+		System.out.println("Address: Dell Computer, Bldg 13 ");
+		System.out.println("Date: April 12, 2005");
+		System.out.print('\n');
+		System.out.println("To: Jack Jones");
+		System.out.print('\n');
+		System.out.println("Message: Help! I'm trapped inside a computer!");
+	
+
+	}
+}
