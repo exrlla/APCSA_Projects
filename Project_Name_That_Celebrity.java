2b0d4f36f1c96fc152cdf81f46489411dd8806d2
Estrella Popoca <65576812+exrlla@users.noreply.github.com>
2020-08-27 12:54:29 -0500

    Add files via upload

---
+++ Project_Name_That_Celebrity.java
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Tester 2
+// Project... Name that Celebrity
+// Estrella Popoca 
+// August 27, 2020
+{
+	public static void main(String[] args) {
+	  
+	  String s1 = "Allan Alda";
+	  String s2 = "John Wayne";
+	  String s3 = "Gregory Peck";
+	  
+	  String AlAld = s1.substring(2, 7);
+	  System.out.println(s1 + ">>>" + AlAld);
+	  
+	  String JWayde = s2.substring(2, 7);
+	  System.out.println(s2 + ">>>" + JWayde);
+	  
+	  String GregP = s3.substring(2, 9);
+	  System.out.println(s3 + ">>>" + GregP );
+}
+}
