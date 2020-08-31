37a4c388914cc46e6971a304367ab190b262a436
Estrella Popoca <65576812+exrlla@users.noreply.github.com>
2020-08-31 10:17:54 -0500

    Add files via upload

---
+++ Project_Arithmetic_Assignment.java
@@ -0,0 +1,22 @@
+// Name: Estrella Popoca
+// Date: August 31, 2020 
+
+public class Main
+{ 
+    public static void main(String args[])
+    {
+      int p;
+      p = 79+3*(4+82-68)-7+19;
+      System.out.println("79 + 3 * (4 + 82 - 68) - 7 + 19 = " + p);
+      
+      int s;
+      s = (179+21+10)/7+181;
+      System.out.println("(179 + 21 + 10) / 7 + 181 = " + s);
+      
+      int u;
+      u = 10389*56*11+2246;
+      System.out.println("10389 + 56 * 11 + 2246 = " + u);
+    
+    }
+    
+}
